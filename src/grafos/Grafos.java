02afde73fed947cf81be5a19f54fc8586d4c4b47
David Flores Silva <dfloressi@unsa.edu.pe>
2021-08-03 10:57:55 -0500

    se creo el metodo unir que unira las aristas con un peso indicado aun nos faltaria agregar funcion para sus respectivos parametros

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -19,11 +19,11 @@ public class Grafos {
 	}
 	
 	/*
-	 * metodo que clona un grafo
-	 * los primero que hace es crear un objeto de la misma clase
-	 * luego renombra los datos de las variables
-	 * su numero de vertices y el numero de aristas
-	 * y retorna un nuevo grafo que es clonado 
+	  metodo que clona un grafo
+	  los primero que hace es crear un objeto de la misma clase
+	  luego renombra los datos de las variables
+	  su numero de vertices y el numero de aristas
+	  y retorna un nuevo grafo que es clonado 
 	*/
 	public Grafos clonar() { 
 		Grafos g = new Grafos();
@@ -39,7 +39,60 @@ public class Grafos {
 	}
 	
 	
-	//
+	
+	
+	
+	
+	
+	
+	/*metodo que une dos aristas
+		como primer parametros recibe un desde que es la arista de inicio
+		como segundo parametro recibe una arista de destino 
+		como tercer paremetro recibe el costo que hay de ir desde
+		el primer parametro hasta el segundo parametro 
+	*/
+	public int unirAristas(int desde, int hasta, int costo) {
+		Arista newArista = new Arista(desde, hasta, costo);
+		unirAristas(desde, hasta, newArista);
+		return ++numAristas;
+	}
+	
+	
+	/*metodo que une dos aristas
+		como primer parametros recibe un desde que es la arista de inicio
+		como segundo parametro recibe una arista de destino 
+		como tercer paremetro recibe un valor de tipo Arista que es la arista que se tendra que conectar
+	 */
+	public void unirAristas(int desde, int hasta, Arista a) {
+		if( desde >= cantVertices() ) {
+			lista_vert.setSize(desde+1);
+		}
+		
+		if( hasta >= cantVertices() ) {
+			lista_vert.setSize(hasta+1);
+		}
+		
+		if( vertice(desde) == null ) {
+			lista_vert.set(desde, new Vertice(null));
+		}
+		
+		if( vertice(hasta) == null ) {
+			lista_vert.set(hasta, new Vertice(null));
+		}
+		
+		vertice(desde).unir(a);
+	}
+	
+	
+	public void separarAristas(int desde, int hasta) {
+		if(cantVertices() > desde) {
+			vertice(desde).separarAristas(hasta);
+		}
+	}
+	
+	
+	
+	
 	
 	
 }	
